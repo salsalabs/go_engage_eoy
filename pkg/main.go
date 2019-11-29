d6e4f5767940e448b66dc5fda9810f162138f920
Basledi <basledi@posteo.net>
2019-11-29 11:28:43 -0600

    Standardized styles for data types.

--- pkg/main.go
+++ pkg/main.go
@@ -10,6 +10,18 @@ import (
 	goengage "github.com/salsalabs/goengage/pkg"
 )
 
+//Runtime contains the variables that we need to run this application.
+type Runtime struct {
+	Env         *goengage.Environment
+	DB          *gorm.DB
+	Log         *log.Logger
+	Channels    []chan goengage.Fundraise
+	Spreadsheet *excelize.File
+	CountStyle  int
+	ValueStyle  int
+	KeyStyle    int
+}
+
 //ActivityForm contains a basic set of values for an activity page.
 type ActivityForm struct {
 	ID          string
@@ -17,8 +29,8 @@ type ActivityForm struct {
 	CreatedDate *time.Time
 }
 
-//GivingStat is used to store the usual statistics about a set of donations.
-type GivingStat struct {
+//Stat is used to store the usual statistics about a set of donations.
+type Stat struct {
 	// supporterID, activityPageID, Year, Year-Month
 	ID              string
 	AllCount        int32
@@ -45,19 +57,29 @@ type Month struct {
 	CreatedDate *time.Time
 }
 
-//Runtime contains the variables that we need to run this application.
-type Runtime struct {
-	Env         *goengage.Environment
-	DB          *gorm.DB
-	Log         *log.Logger
-	Channels    []chan goengage.Fundraise
-	Spreadsheet *excelize.File
-}
-
 //Year is used to provide a primary key for storing stats by year.
 type Year struct {
 	ID          int
 	CreatedDate *time.Time
+}
+
+//Decorator describes how all of the sheets should behave.  An "index" is the
+//offset from the start of the results returned by the database, where descriptive
+//values (from the keys) are followed by a stats object.
+type Decorator interface {
+	//Index is the offset from the beginning of the results
+	//returned by the database.  Keys are first, stats follow.
+	Value(index int) interface{}
+	Style(index int) string
+	Axis(row, index int) string
+}
+
+//Sheet contains the stuff that we need to create and populate a sheet
+//in the EOY spreadsheet.
+type Sheet struct {
+	Name   string
+	Keys   []string
+	Titles []string
 }
 
 //NewRuntime creates a runtime object and initializes the rt.
@@ -67,7 +89,9 @@ func NewRuntime(e *goengage.Environment, db *gorm.DB, channels []chan goengage.F
 		log.Panic(err)
 	}
 	s := excelize.NewFile()
-	s.DeleteSheet("Sheet1")
+	countStyle, _ := s.NewStyle(`{"number_format": 3}`)
+	valueStyle, _ := s.NewStyle(`{"number_format": 3}`)
+	keyStyle, _ := s.NewStyle(`{"number_format": 0}`)
 
 	rt := Runtime{
 		Env:         e,
@@ -75,6 +99,9 @@ func NewRuntime(e *goengage.Environment, db *gorm.DB, channels []chan goengage.F
 		Log:         log.New(w, "EOY: ", log.LstdFlags),
 		Channels:    channels,
 		Spreadsheet: s,
+		CountStyle:  countStyle,
+		ValueStyle:  valueStyle,
+		KeyStyle:    keyStyle,
 	}
 
 	return &rt
